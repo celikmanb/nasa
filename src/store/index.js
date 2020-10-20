9325f3bf0e2f79bbe83b21dd71d5bcb5cb9ce15c
celikmanb <bcelikman@gmail.com>
2020-10-20 13:59:01 +0300

    Router and store created

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Nasa from './modules/nasa/nasa.store'
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    Nasa
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <h2 class="text-center mb-3">Nasa Public API</h2>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <h2>Daily APOD Image</h2>
+        <p><b>Image Title: </b>{{this.apod_data.title}}</p>
+        <p><b>Explanation: </b>{{this.apod_data.explanation}}</p>   
+      </v-col>
+      <v-col cols="6">
+        <v-img :src="this.apod_data.url"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    
+  }),
+  computed: {
+      ...mapState({
+        apod_loading: state => state.Nasa.apod_loading,
+        apod_data: state => state.Nasa.apod_data,
+        apod_error: state => state.Nasa.apod_error
+      })
+    },
+  methods: {
+    ...mapActions({
+      fetchApod: 'Nasa/fetchApod'
+    }),
+  },
+  created() {
+    this.fetchApod();
   }
 }
 </script>
